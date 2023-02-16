34944688baeb6c3024a855a0b2de083682f1603e
amin subagiyo <aminsum9@gmail.com>
2023-02-16 16:42:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = React.useState({
+    cartA: [
+      "apel",
+      "nanas",
+      "pisang"
+    ],
+    cartB: [
+      "jeruk",
+      "manggis",
+      "strawberi"
+    ]
+  })
+
+
+  const handleMoveFruit = async (item, cart) => {
+    if (cart == 'a') {
+      //
+      var newCartA = await state.cartA.filter((e) => {
+        return e != item
+      })
+
+      var newCartB = [...state.cartB, item];
+
+      setState(prevState => ({ ...prevState, cartA: newCartA, cartB: newCartB }));
+
+    } else if (cart == 'b') {
+
+      var newCartB = await state.cartB.filter((e) => {
+        return e != item
+      })
+
+      var newCartA = [...state.cartA, item];
+
+      setState(prevState => ({ ...prevState, cartA: newCartA, cartB: newCartB }));
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cart-a">
+        {state.cartA.map((item, index) => {
+          return (
+            <div style={{color: index % 2 ? "#fff" : "#000"}} onClick={() => handleMoveFruit(item, 'a')}>
+              <h3>{item}</h3>
+            </div>
+          )
+        })}
+      </div>
+      <div className="cart-b">
+        {state.cartB.map((item, index) => {
+          return (
+            <div style={{color: index % 2 ? "#fff" : "#000"}}  onClick={() => handleMoveFruit(item, 'b')}>
+              <h3>{item}</h3>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
